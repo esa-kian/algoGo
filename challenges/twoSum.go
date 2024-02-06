3005425ec926875e088f46b49fc1c63d816d205b
Ehsan A. Kian <ehsan_amd72@hotmail.com>
2024-02-06 14:29:37 +0000

    desc two sums

--- challenges/twoSum.go
+++ challenges/twoSum.go
@@ -1,3 +1,10 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
 func twoSum(nums []int, target int) []int {
     var result []int
     for i := 0; i < len(nums)-1; i++ {
